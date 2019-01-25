2d1297a4d1a5ae14d871498605b14553f4627dee
Mathew <bigdogmat123@gmail.com>
2019-01-24 21:41:09 -0500

    Push initial files

---
+++ lua/autorun/egpurlmaterial_load.lua
@@ -0,0 +1,78 @@
+if CLIENT then
+  file.CreateDir "egpurlmaterial"
+
+  hook.Add("ShutDown", "EGPURLMATERIAL_CLEANUP", function()
+    for k, name in ipairs(file.Find("egpurlmaterial/*", "DATA")) do
+      file.Delete("egpurlmaterial/" .. name)
+    end
+  end)
+end
+
+hook.Add("Initialize", "EGPURLMATERIAL_LOAD", function()
+  if not EGP then
+    ErrorNoHalt "[EGPUrlMaterial]: Wiremod is either not installed or hasn't loaded correctly"
+    return
+  end
+
+  if SERVER then return end
+
+  local urlMatLookup = {}
+
+  function EGP:ReceiveMaterial(tbl)
+    local temp = net.ReadString()
+    local what, mat = temp:sub(1,1), temp:sub(2)
+    if what == "0" then
+    if mat:sub(1, 4) == "http" then
+        local name, ent = debug.getlocal(4, 3)
+        if name ~= "Ent" or not isentity(ent) then
+          ent = nil
+          ErrorNoHalt "[EGPUrlMaterial]: [ENT] This addons needs to be updated to work with the latest version of wiremod"
+        end
+
+        local name, obj = debug.getlocal(3, 1)
+        if name ~= "self" or not istable(obj) then
+          obj = nil
+          ErrorNoHalt "[EGPUrlMaterial]: [OBJ] This addons needs to be updated to work with the latest version of wiremod"
+        end
+
+        local extension = string.match(mat, ".+(%..+)$")
+
+        if ent and obj and extension then
+          http.Fetch(mat, function(body)
+            if not IsValid(ent) then return end
+
+            local hash = util.CRC(body)
+            local fileName = "egpurlmaterial/" .. tostring(hash) .. extension
+            print(fileName)
+
+            if not file.Exists(fileName, "DATA") then
+              file.Write(fileName, body)
+            end
+
+            if not urlMatLookup[hash] then
+              urlMatLookup[hash] = Material("../data/" .. fileName, "smooth")
+            end
+
+            print(urlMatLookup[hash])
+
+            obj.material = urlMatLookup[hash]
+            ent:EGP_Update()
+          end)
+        end
+
+        tbl.material = false
+      elseif mat == "" then
+        tbl.material = false
+      else
+        tbl.material = Material(mat)
+      end
+    elseif what == "1" then
+      local num = tonumber(mat)
+      if not num or not IsValid(Entity(num)) then
+        tbl.material = false
+      else
+        tbl.material = Entity(num)
+      end
+    end
+  end
+end)
